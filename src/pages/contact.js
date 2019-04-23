8e91c33c916eaeb116bbb83125a8f4e3732907a5
dienigma <joshi.chinmay09@gmail.com>
2019-04-23 20:55:58 +0530

    Built basic pages

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>Email: joshi.chinmay09@gmail.com</p>
+      <p>Mobile No: +91 8530134149</p>
+      <p>
+        <a href="https://github.com/dienigma">Github</a>
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I am Chinmay. I am a full stack developer, living in Hyderabad.</h2>
+      <p>
+        Need a developer? <a href="/contact">Contact Me</a>
+      </p>
+      <p>
+        Need a developer? <Link to="/contact">Contact me</Link>
+      </p>
+    </div>
+  )
+}
+
+export default IndexPage
