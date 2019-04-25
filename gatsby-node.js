a49779b613dd48d4fd93e097c2fc298c285032dc
dienigma <joshi.chinmay09@gmail.com>
2019-04-25 13:23:34 +0530

    fixing slug bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
   if (node.internal.type === "MarkdownRemark") {
     const slug = path.basename(node.fileAbsolutePath, ".md")
+
     createNodeField({
       node,
       name: "slug",
@@ -13,33 +14,30 @@ module.exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-// const createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   const blogTemplate = path.resolve("./src/templates/src/blog.js")
-//   const response = await graphql(`
-//     allMarkdownRemark{
-//       edges{
-//         node{
-//           fields{
-//             slug
-//           }
-//         }
-//       }
-//     }
-//   `)
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blog.js")
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
 
-//   response.data.allMarkdownRemark.edges.foreEach(edge => {
-//     createPage({
-//       component: blogTemplate,
-//       path: `/blog/${edge.node.fields.slug}`,
-//       context: {
-//         slug: edge.node.fields.slug,
-//       },
-//     })
-//   })
-
-// Get path to template.
-
-// get markdown data
-// create new pages.
-// }
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
