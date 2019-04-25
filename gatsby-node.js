8c6dfddab3688c23908c8201a475a7b35d531aa0
dienigma <joshi.chinmay09@gmail.com>
2019-04-25 12:36:07 +0530

    Slug has bugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,23 +13,34 @@ module.exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-module.exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogTemplate = path.resolve("./src/templates/src/blog.js")
-  graphql(`
-    allMarkdownRemark{
-      edges{
-        node{
-          fields{
-            slug
-          }
-        }
-      }
-    }
-  `)
+// const createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogTemplate = path.resolve("./src/templates/src/blog.js")
+//   const response = await graphql(`
+//     allMarkdownRemark{
+//       edges{
+//         node{
+//           fields{
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   response.data.allMarkdownRemark.edges.foreEach(edge => {
+//     createPage({
+//       component: blogTemplate,
+//       path: `/blog/${edge.node.fields.slug}`,
+//       context: {
+//         slug: edge.node.fields.slug,
+//       },
+//     })
+//   })
 
   // Get path to template.
 
   // get markdown data
   // create new pages.
 }
+
