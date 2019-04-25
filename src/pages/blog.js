00c4cc73507c6d2e34393be0a8b6852572205b42
dienigma <joshi.chinmay09@gmail.com>
2019-04-25 20:00:57 +0530

    Setup Content Ful

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql, useStaticQuery } from "gatsby"
-
+import { graphql, useStaticQuery, Link } from "gatsby"
+import blogStyles from "./blog.module.scss"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -14,6 +14,9 @@ const BlogPage = () => {
             }
             html
             excerpt
+            fields {
+              slug
+            }
           }
         }
       }
@@ -23,17 +26,19 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ul>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
-              <p>{edge.node.excerpt}</p>
+            <li className={blogStyles.post}>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+                <p>{edge.node.excerpt}</p>
+              </Link>
             </li>
           )
         })}
-      </ul>
+      </ol>
     </Layout>
   )
 }
