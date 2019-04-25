b32e8431a314388ce7b08ce67d26333c611ee468
dienigma <joshi.chinmay09@gmail.com>
2019-04-25 22:24:58 +0530

    Little changes to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>I am Chinmay. I am a full stack developer, living in Hyderabad.</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me</Link>
-      </p>
     </Layout>
   )
 }
