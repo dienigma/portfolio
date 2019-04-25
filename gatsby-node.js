49da2154c5542522f4b06f45c212193f4c706dba
dienigma <joshi.chinmay09@gmail.com>
2019-04-25 12:13:48 +0530

    Got the slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,35 @@
+const path = require("path")
+
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
 
-  console.log(JSON.stringify(node, undefined, 2))
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
+
+module.exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/src/blog.js")
+  graphql(`
+    allMarkdownRemark{
+      edges{
+        node{
+          fields{
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  // Get path to template.
+
+  // get markdown data
+  // create new pages.
 }
